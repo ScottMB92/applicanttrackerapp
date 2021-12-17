663ecf589785217e757058572900bb7899c0c5b3
Scott Boulter <Scott.Boulter@transformingsupport.uk>
2021-12-17 12:40:59 +0000

    Significant changes made

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const mongoose = require ('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect('mongodb+srv://admin-scott:test123@cluster0.wtohy.mongodb.net/newapplicantsDB?retryWrites=true&w=majority');
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin-scott:test123@cluster0.wtohy.mongodb.net/newapplicantsDB?retryWrites=true&w=majority');
 
 const applicantSchema = {
     name: String,
@@ -68,6 +71,13 @@ app.delete('/delete/:id', (request, response) => {
         }
       });
     });
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get("*", (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+};
 
 app.listen(port, function () {
     console.log("Server is running on port 4000");
